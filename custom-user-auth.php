198ddcbce5dc64793447ee8fbc7f02b8b37cce26
julymoe <athelay.lwin@gmail.com>
2024-07-14 20:48:31 +0700

    add setting link

--- custom-user-auth.php
+++ custom-user-auth.php
@@ -19,4 +19,14 @@ require_once plugin_dir_path(__FILE__) . 'cua-registration.php';
 require_once plugin_dir_path(__FILE__) . 'cua-login.php';
 require_once plugin_dir_path(__FILE__) . 'cua-admin.php';
 
+// Add a settings link to the plugins page
+function cua_add_settings_link($links)
+{
+    $settings_link = '<a href="admin.php?page=cua-settings">' . __('Settings', 'cua') . '</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+}
+
+// Replace {plugin_file} with the actual main plugin file name
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'cua_add_settings_link');
 ?>
